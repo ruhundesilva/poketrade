07eed51fde6f886204db5dca1f525182b36f3a67
Jmar-T <jtaylor448@gatech.edu>
2025-04-25 21:23:04 -0400

    updated signup to include email for forgotten password

---
+++ home/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms.utils import ErrorList
+from django.utils.safestring import mark_safe
+from django.contrib.auth.models import User
+
+class CustomErrorList(ErrorList):
+    def __str__(self):
+        if not self:
+            return ''
+        return mark_safe(''.join([
+            f'<div class="alert alert-danger" role="alert">{e}</div>' for e in self]))
+
+class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        for fieldname in ['username', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+            self.fields[fieldname].widget.attrs.update(
+                {'class': 'form-control'}
+            )
+            
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')  
